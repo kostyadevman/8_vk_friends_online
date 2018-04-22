14c2d0224c203ecd2a03121ff61f182fcc785fdf
kostya <kostra@localdomain>
2018-04-22 20:47:26 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Watcher of Friends Online
 
-[TODO. There will be project description]
+This program prints to the console your online friends in social network vk.com
 
 # How to Install
 
@@ -10,6 +10,18 @@ Python 3 should be already installed. Then use pip (or pip3 if there is a confli
 pip install -r requirements.txt # alternatively try pip3
 ```
 
+
+To work you need to register your application at https://vk.com/dev and insert your application_id in variable name APP_ID.
+
+ Example:
+````bash
+$ python3 vk_friends_online.py
+Enter you login: login@yandex.ru
+Enter password: ********
+Иван Иванов
+Петр Петров
+
+````
 Remember, it is recommended to use [virtualenv/venv](https://devman.org/encyclopedia/pip/pip_virtualenv/) for better isolation.
 
 # Project Goals
--- vk_friends_online.py
+++ vk_friends_online.py
@@ -1,32 +1,43 @@
 import vk
 
-
-APP_ID = -1  # чтобы получить app_id, нужно зарегистрировать своё приложение на https://vk.com/dev
+APP_ID = 1234567
 
 
 def get_user_login():
-    pass
+    return input('Enter login: ')
 
 
 def get_user_password():
-    pass
+    return input('Enter password: ')
 
 
 def get_online_friends(login, password):
     session = vk.AuthSession(
         app_id=APP_ID,
-        user_login=login,
-        user_password=password,
+        user_login = login,
+        user_password = password,
+        scope = 'friends'
     )
     api = vk.API(session)
-    # например, api.friends.get()
+    online_friends_ids = api.friends.getOnline()
+    friends_online = api.usersget(
+        user_ids = online_friends_ids,
+        fields = ['last_name', 'first_name']
+    )
+    return friends_online
 
 
-def output_friends_to_console(friends_online):
-    pass
+def print_friends_to_console(friends_online):
+    print('Friends online: ')
+    for friend in friends_online:
+        print('{} {}'.format(
+            friend.get('first_name'),
+            friend.get('last_name'))
+        )
+
 
 if __name__ == '__main__':
     login = get_user_login()
     password = get_user_password()
     friends_online = get_online_friends(login, password)
-    output_friends_to_console(friends_online)
+    print_friends_to_console(friends_online)
